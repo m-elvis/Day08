d69f96b526b8ceb9e5aa163d9e9a0305a8c1820b
m-elvis <elvis.mahovic@gmail.com>
2021-03-15 23:08:27 +0100

    undefined

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+    console.log("addThree: " + sum);
+}
+
+function addFive() {
+ sum = sum + 5;
+ console.log("addFive: " + sum);
+}
+
+addThree();
+addFive();
+
+module.exports = {
+    addThree,
+    addFive,
+}
+
