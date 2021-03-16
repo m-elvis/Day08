f56f3f3792acdf2b7ef70995809a0ee75dd4bcb9
m-elvis <elvis.mahovic@gmail.com>
2021-03-16 01:22:26 +0100

    strtoint

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -2,5 +2,5 @@ function randomRangeNumber(minNumber, maxNumber) {
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 }
 
-console.log(randomRangeNumber(1, 34));
+console.log(randomRangeNumber(1, 5));
 module.exports = randomRangeNumber;
---
+++ ex14/src/index.js
@@ -0,0 +1,8 @@
+function convertStrToInt (str) {
+return parseInt(str);
+}
+convertStrToInt();
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"))
+module.exports = convertStrToInt;
